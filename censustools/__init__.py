c26d6f96efdf4d01aa45e9e047d82f143eee8558
Paul Ulrich <pulrich@gsu.edu>
2024-06-18 12:07:17 -0400

    minor changes to census imports in __init__.py

--- censustools/__init__.py
+++ censustools/__init__.py
@@ -1,2 +1,2 @@
-from .censusdata import tract_income_poverty, fetch_tract_data, optimized_tract_income_poverty
+from .censusdata import fetch_tract_data, optimized_tract_income_poverty, patch_census_library, tract_income_poverty
 from .geocoding import batch_geocode
