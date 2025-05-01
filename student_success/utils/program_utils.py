ec526280c664cf33852e50671615de8ae11c3e57
Paul Ulrich <pulrich@gsu.edu>
2025-05-01 11:43:17 -0400

    operationalized premajor abbreviation replacement

--- student_success/utils/program_utils.py
+++ student_success/utils/program_utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from student_success.utils.constants import MAJOR_ABBREV_TO_LABEL, MAJOR_TO_DISCIPLINE_DICT
+from student_success.utils.constants import MAJOR_ABBREV_TO_LABEL, MAJOR_TO_DISCIPLINE_DICT, PREMAJOR_TO_MAJOR_DICT
 
 
 def classify_discipline(major):
@@ -45,8 +45,38 @@ def lookup_major_name(major):
     --------
     Full names are determined by the `MAJOR_ABBREV_LABEL` dictionary.
     This dictionary can be modified to reflect your institution's specific majors.
+    Consider processing with replace_premajor_abbreviations befordhand!
     """
 
     if pd.isna(major):
         return np.nan  # Return NaN for missing values
     return MAJOR_ABBREV_TO_LABEL.get(major, 'Other')
+
+
+def replace_premajor_abbreviations(major):
+    """
+    Returns abbreviation for the "mature" major given abbreviation for a premajor using a predefined mapping .
+
+    This function takes a major abbreviation as input and returns the corresponding
+    abbreviation of the "mature" major using a predefined dictionary `PREMAJOR_TO_MAJOR_DICT`. If the
+    abbreviation is not found in the dictionary, the function returns major.
+    If the input is missing (NaN), it returns NaN.
+    Parameters:
+    -----------
+    major : str or NaN
+        The abbreviation of the premajor or major (e.g., 'PNUR', 'BNUR', etc.).
+
+    Returns:
+    --------
+    str or NaN
+        The abbreviation of the "mature" major corresponding to a  premajor abbreviation.
+
+    Notes:
+    --------
+    Abbreviations for the "mature" majors are determined by `PREMAJOR_TO_MAJOR_DICT`.
+    This dictionary can be modified to reflect your institution's specific majors.
+    """
+
+    if pd.isna(major):
+        return np.nan  # Return NaN for missing values
+    return PREMAJOR_TO_MAJOR_DICT.get(major, major)
